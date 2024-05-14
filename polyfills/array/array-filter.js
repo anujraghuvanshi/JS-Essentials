4914dbb6c2487013bf2dc28ee5a11f1a4aff7c3b
Anuj Raghuvanshi <22232709+anujraghuvanshi@users.noreply.github.com>
2024-05-15 03:16:06 +0530

    Update array-filter.js

--- polyfills/array/array-filter.js
+++ polyfills/array/array-filter.js
@@ -1,6 +1,5 @@
 const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
-
 Array.prototype.myFilter = function (callback, ...args) {
   if (!Array.isArray(this)) {
     throw new Error('Array.prototype.myFilter must be called on array')
