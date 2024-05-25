56f70a5086d575dd10bc8746b98409a13aed8683
Anuj Raghuvanshi <22232709+anujraghuvanshi@users.noreply.github.com>
2024-05-26 02:59:39 +0530

    🚀 Created Array.entries() polyfill

---
+++ polyfills/array/array-entries.js
@@ -0,0 +1,21 @@
+const myArray = [5, 10, 15, 20, 25, 30, 35, 40]
+
+Array.prototype.myEntries = function (index) {
+  if (!Array.isArray(this)) {
+    throw new Error('Array.prototype.myEntries must be called on array')
+  }
+
+  const iteratorArray = this.map((item, index) => [index, item])
+
+  function* iterator() {
+    yield* iteratorArray;
+  }
+
+  return iterator()
+}
+
+const result = myArray.myEntries()
+
+// Output here 
+console.log('result (Iteration 1) => \n', result.next().value)
+console.log('result (Iteration 2) => \n', result.next().value)
