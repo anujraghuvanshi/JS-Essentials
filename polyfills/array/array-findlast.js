e46dc69fef6a959749428996795c462171c17cc1
Anuj Raghuvanshi <22232709+anujraghuvanshi@users.noreply.github.com>
2024-05-27 01:19:41 +0530

    Added polyfill for Array.findLast()

---
+++ polyfills/array/array-findlast.js
@@ -0,0 +1,22 @@
+const myArray = [5, 12, 130, 50, 44];
+
+Array.prototype.myFindLast = function (callback) {
+  if (!Array.isArray(this)) {
+    throw new Error('Array.prototype.myFindLast must be called on array')
+  }
+
+  if (typeof callback !== 'function') {
+    throw new Error('callback must be a function')
+  }
+
+
+  for (let i = this.length; i > 0; i--) {
+    if (callback(this[i], i, this)) return this[i]
+  }
+
+}
+
+const result = myArray.myFindLast(v => v > 200)
+
+// Output here 
+console.log('result => \n', result)
