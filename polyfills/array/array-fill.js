dff1ece40a029c678c57f8128a6e6b33df5bcc06
Anuj Raghuvanshi <22232709+anujraghuvanshi@users.noreply.github.com>
2024-05-27 00:51:30 +0530

    🚀 Added Polyfill for Array.fill()

---
+++ polyfills/array/array-fill.js
@@ -0,0 +1,26 @@
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+Array.prototype.myFill = function (value = undefined, start = 0, end = 0) {
+  if (!Array.isArray(this)) {
+    throw new Error('Array.prototype.myFill must be called on array')
+  }
+
+  if (start < 0) {
+    start = start + this.length
+  }
+
+  if (end <= 0) {
+    end = end + this.length
+  }
+
+  for (let i = start; i < end; i++) {
+    this[i] = value
+  }
+
+  return this
+}
+
+const result = myArray.myFill(1, -2)
+
+// Output here 
+console.log('result => \n', result)
