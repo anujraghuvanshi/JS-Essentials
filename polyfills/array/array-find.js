fbc89e4fac7dc932ca3e73b3933efe0c5728cffd
Anuj Raghuvanshi <22232709+anujraghuvanshi@users.noreply.github.com>
2024-05-27 00:58:14 +0530

    🚀 Added polyfill for Array.find()

---
+++ polyfills/array/array-find.js
@@ -0,0 +1,20 @@
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+Array.prototype.myFind = function (callback) {
+  if (!Array.isArray(this)) {
+    throw new Error('Array.prototype.myFind must be called on array')
+  }
+
+  if (typeof callback !== 'function') {
+    throw new Error('callback must be a function')
+  }
+
+  for (let i = 0; i < this.length; i++) {
+    if (callback(this[i], i, this)) return this[i]
+  }
+}
+
+const result = myArray.myFind(v => v * 2 == 2)
+
+// Output here 
+console.log('result => \n', result)
