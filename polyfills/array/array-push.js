6ea9bfc491a1fb559c109919a9cfe0b54853a36a
Anuj Raghuvanshi <22232709+anujraghuvanshi@users.noreply.github.com>
2024-05-29 02:51:52 +0530

    🚀 Added polyfill for Array.push()

---
+++ polyfills/array/array-push.js
@@ -0,0 +1,16 @@
+const myArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+Array.prototype.myPush = function () {
+  if (!Array.isArray(this)) {
+    throw new Error('Array.prototype.myPush must be called on array')
+  }
+  for (let i = 0; i < arguments.length; i++) {
+    this[this.length] = arguments[i];
+  }
+  return this.length;
+}
+
+myArray.myPush(11, 12)
+
+// Output here 
+console.log('result (New Array) =>', myArray)
