de331ee2d3a093ffa7a25585f753416b9c65ea3d
Anuj Raghuvanshi <22232709+anujraghuvanshi@users.noreply.github.com>
2024-05-29 02:37:32 +0530

    🚀 Added polyfill for Array.lastIndexOf()

---
+++ polyfills/array/array-lastIndexOf.js
@@ -0,0 +1,19 @@
+const myArray = [5, 12, 44, 130, 50, 44];
+
+Array.prototype.myLastIndexOf = function (value, fromIndex = this.length - 1) {
+  if (!Array.isArray(this)) {
+    throw new Error('Array.prototype.myLastIndexOf must be called on array')
+  }
+
+  for (let i = fromIndex; i >= 0; i--) {
+    if (this[i] === value) return i
+  }
+
+}
+
+const result = myArray.myLastIndexOf(44)
+const result2 = myArray.myLastIndexOf(44, 4)
+
+// Output here 
+console.log('result => \n', result)
+console.log('result2 => \n', result2)
