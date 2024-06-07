c342a24c84bc462c0ad0d7bc286b04e5ef28c825
Anuj Raghuvanshi <22232709+anujraghuvanshi@users.noreply.github.com>
2024-06-08 03:01:04 +0530

    🚀 Created Array.reduce() polyfill

---
+++ polyfills/array/array-reduce.js
@@ -0,0 +1,34 @@
+const myArray = [1, 2, 3, 4]
+
+Array.prototype.myReduce = function (cb, initialValue) {
+  // Throw Error if function is not called on array.
+  if (!Array.isArray(this)) {
+    throw new Error('Array.prototype.myReduce must be called on array')
+  }
+
+  /**
+   * ORIGINAL LOGIC STARTS HERE
+   */
+  let result;
+  let startIndex = 0;
+
+  if (!initialValue) {
+    result = this[0]
+    startIndex = 1
+  }
+
+  if (arguments.length >= 2) {
+    result = initialValue
+  }
+
+  for (i = 0; i < this.length; i++) {
+    result = cb(result, this[i])
+  }
+
+  return result;
+}
+
+const result = myArray.myReduce((acc, value) => acc + value, 0)
+
+// Output here 
+console.log('result => \n', result)
