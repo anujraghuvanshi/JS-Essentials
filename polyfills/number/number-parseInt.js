dd5db27fb6e9ae073b6325ab28f292a1dba09514
Anuj Raghuvanshi <22232709+anujraghuvanshi@users.noreply.github.com>
2024-06-29 03:36:36 +0530

    🚀 Added polyfill for Number.parseInt() (#24)

---
+++ polyfills/number/number-parseInt.js
@@ -0,0 +1,23 @@
+Number.myParseInt = function (str, radix) {
+  str = String(str).trim();
+
+  if (!radix) {
+    radix = 10;
+  }
+
+  if (str) {
+    var result = parseInt(str, radix);
+    return isNaN(result) ? NaN : result;
+  }
+
+  return NaN;
+}
+
+console.log(Number.myParseInt('42'));
+console.log(Number.myParseInt('   42'));
+console.log(Number.myParseInt('42abc'));
+console.log(Number.myParseInt('abc42'));
+console.log(Number.myParseInt('101', 2));
+console.log(Number.myParseInt('0xFF', 16));
+console.log(Number.myParseInt(''));
+console.log(Number.myParseInt(' '));
